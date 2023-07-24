d5fa71a08c2846bfdaabbcc560360c7b96c0947c
ZacharyC <129316897+ZacharyChandonnet@users.noreply.github.com>
2023-07-23 20:37:41 -0400

    preloader + tablet component

--- js/App.js
+++ js/App.js
@@ -2,9 +2,11 @@
  * @module App
  */
 export default class App {
-    
+
     static main() {
         this.hamMenu();
+        this.nav();
+        this.title();
     }
     /**
      * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
@@ -23,6 +25,37 @@ export default class App {
         menuHamburger.addEventListener('click', () => {
             navLinks.classList.toggle('mobile-menu')
         })
+    }
+
+    static nav() {
+        document.querySelectorAll('a[data-scroll]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    }
+
+    static title() {
+
+        gsap.to('.title', {
+            delay:2,
+            y: 0,
+            duration: 4,
+            opacity: 1,
+            ease: "power4.easeIn",
+        });
+
+        gsap.from('.navbar', {
+            delay: 1,
+            y: -100,
+            duration: 2,
+            opacity: 0,
+            ease: "power4.easeIn",
+        });
     }
 }
 App.init();
